ad34b8793b5eda390f2777b9cab0f59168276229
Dang Van Hung <Hung.DangVan@nttdata.com>
2018-04-12 18:57:16 +0700

    First commit

---
+++ react-flux/src/components/homePage.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react');
+var Home = React.createClass({
+    render: function () {
+        return (
+            < div className="jumbotron">
+                <h1> Plural sight administration</h1>
+                <p> react , react router and flux</p>
+            </div>
+        );
+    }
+});
+module.exports = Home;
---
+++ react-flux/src/main.js
@@ -0,0 +1,6 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
+
+React.render(<Home/>, document.getElementById('app'));
